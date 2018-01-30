8900ca331ef6b2a6bd5db6809e3659560893bc5d
moi <moi@moi.com>
2018-01-30 12:44:48 +0100

    activation/desactivation du mode tri

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,63 +10,63 @@ export class HomePage {
   animals = [
     {
       'title': 'Vache',
-      'image': 'img/animals/cow-icon.png',
+      'image': 'imgs/animals/cow-icon.png',
       'desc': 'Meugle',
       'file': '/sounds/cow.mp3',
       'playing': false
     },
     {
       'title': 'Dauphin',
-      'image': 'img/animals/dolphin-icon.png',
+      'image': 'imgs/animals/dolphin-icon.png',
       'desc': 'Siffle',
       'file': '/sounds/dolphin.mp3',
       'playing': false
     },
     {
       'title': 'Grenouille',
-      'image': 'img/animals/frog-icon.png',
+      'image': 'imgs/animals/frog-icon.png',
       'desc': 'Coasse',
       'file': '/sounds/frog.mp3',
       'playing': false
     },
     {
       'title': 'Oiseau',
-      'image': 'img/animals/bird-icon.png',
+      'image': 'imgs/animals/bird-icon.png',
       'desc': 'Chante',
       'file': '/sounds/bird.mp3',
       'playing': false
     },
     {
       'title': 'Cochon',
-      'image': 'img/animals/pig-icon.png',
+      'image': 'imgs/animals/pig-icon.png',
       'desc': 'Grogne',
       'file': '/sounds/pig.mp3',
       'playing': false
     },
     {
       'title': 'Chien',
-      'image': 'img/animals/puppy-icon.png',
+      'image': 'imgs/animals/puppy-icon.png',
       'desc': 'Aboie',
       'file': '/sounds/dog.mp3',
       'playing': false
     },
     {
       'title': 'Chat',
-      'image': 'img/animals/black-cat-icon.png',
+      'image': 'imgs/animals/black-cat-icon.png',
       'desc': 'Miaule',
       'file': '/sounds/cat.mp3',
       'playing': false
     },
     {
       'title': 'Cheval',
-      'image': 'img/animals/horse-icon.png',
+      'image': 'imgs/animals/horse-icon.png',
       'desc': 'Hennit',
       'file': '/sounds/horse.wav',
       'playing': false
     },
     {
       'title': 'Ane',
-      'image': 'img/animals/donkey-icon.png',
+      'image': 'imgs/animals/donkey-icon.png',
       'desc': 'Brait',
       'file': '/sounds/donkey.wav',
       'playing': false
@@ -74,6 +74,7 @@ export class HomePage {
   ];
 
   private currentPosition: number;
+  public result: string;
 
   constructor(public navCtrl: NavController) {
 
@@ -97,7 +98,8 @@ export class HomePage {
    * Lecture d'un son
    */
   playSound() {
-    // Choix d'un son
+    this.result = "";
+    // Choix d'un animal
     this.currentPosition = this.pickAnimalPosition();
     let choosenAnimal = this.animals[this.currentPosition];
     // Chargement du son
@@ -106,6 +108,23 @@ export class HomePage {
     audio.load();
     // Lecture du son
     audio.play();
+  }
+
+  /**
+   * Deviner l'animal en fonction de son cri
+   */
+  guess(pos) {
+    // Test si on a joué un son
+    if (this.currentPosition) {
+      // Test si on a choisi le bon animal
+      if (pos == this.currentPosition) {
+        this.result = "Gagné";
+        // Reinitialisation du choix pour faire un nouveau jeu
+        this.currentPosition = null;
+      } else {
+        this.result = "Essaie encore";
+      }
+    }
   }
 
 }
